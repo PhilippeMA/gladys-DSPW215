39cea5336d5a38bdb258703bfb2b81bc33ee110b
PhilippeMA <philippe.mace@gmail.com>
2017-09-16 17:24:29 +0200

    first commit
    for testing

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function(sails) {
+
+    var setup = require('./lib/setup.js');
+    var exec = require('./lib/exec.js');
+
+    return {
+        //setup: setup,
+        exec: exec
+    };
+};
